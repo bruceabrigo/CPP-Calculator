b9d3fd065021dba6ddce2738b8fb91c7280c197e
Bruce Abrigo <bruceabrigo@outlook.com>
2023-09-07 15:44:13 -0500

    Can now accept user input and switch case (+) operatates

--- calculator.cpp
+++ calculator.cpp
@@ -3,5 +3,24 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world" << endl;
+    char op;
+    float num1,
+        num2;
+    cout << "Welcome to my calculator! \n";
+    cout << "Please enter the first number: \n";
+    cin >> num1;
+    cout << "Thank you! Enter number 2: \n";
+    cin >> num2;
+    cout << "Thank you! Enter an operator: \n";
+    cin >> op;
+
+    switch(op) {
+        case '+':
+            cout << num1 + num2;
+            break;
+        default:
+            cout << "You entered an invalid operator. Please choose from the following (+, -, /, *) \n";
+            break;
+    }
+
 }
